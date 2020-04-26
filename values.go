5f16ecbdfca36aab45999cf7f08538b6ecca2445
kusingh6 <kulpreetsingh.acme@gmail.com>
2020-04-26 14:20:10 -0700

    Updated value fuction

--- values.go
+++ values.go
@@ -11,4 +11,21 @@ func main() {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
+
+	var x float64 = 20.0
+
+	y := 42
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
+
+	var a, b, c = 3, 4, "foo4"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("a is of type %T\n", a)
+	fmt.Printf("b is of type %T\n", b)
+	fmt.Printf("c is of type %T\n", c)
 }
